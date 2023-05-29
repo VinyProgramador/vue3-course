5c646d129f7658c6815e8b3ba8f8904fe34339a5
VinyProgramador <vinicius.nunes@sptech.school>
2023-05-29 14:57:23 -0300

    Requisição http em VueJs3

--- project-quiz-game/src/App.vue
+++ project-quiz-game/src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <HelloWorld msg="Olá mundo!"/>
+  <div>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus reprehenderit obcaecati ut atque iste rem ullam dolores odit sed.</h1>
+    <input type="radio" name="options" value="true">
+    <label>True</label><br>
+    <input type="radio" name="options" value="false">
+    <label>False</label><br>
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+    created(){
+      this.axios.get('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy')
+      .then((response)=>{
+          console.log(response)
+      })
+    }
+
+  //url para o serviço
+  //https://opentdb.com/api.php?amount=1&category=18&difficulty=easy
 }
 </script>
 
@@ -21,5 +33,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 960px;
+  margin: 60px auto;
+
+  input[type=radio]{
+    margin: 12px 4px;
+  }
+
+  button.send{
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #2c3e50;
+    cursor: pointer;
+    border: none;
+  }
 }
 </style>
