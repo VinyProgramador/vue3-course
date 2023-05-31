7b3d5d0c365efe460264850cc11942d69d4ed4e3
VinyProgramador <vinicius.nunes@sptech.school>
2023-05-31 09:00:37 -0300

    renderização das perguntas

--- project-quiz-game/src/App.vue
+++ project-quiz-game/src/App.vue
@@ -1,61 +1,68 @@
 <template>
   <div>
-    <h1 v-html="this.question"></h1>
-    <input type="radio" name="options" value="true">
-    <label>True</label><br>
-    <input type="radio" name="options" value="false">
-    <label>False</label><br>
-    <button class="send" type="button">Send</button>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+
+      <template v-for="(answer, i) in this.answers" :key="i">
+        <input type="radio" name="options" value="answers" />b
+        <label v-html="answer"></label><br />
+      </template>
+      <button class="send" type="button">Send</button>
+    </template>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-data() {
-  return {
-    question: '',
-    incorrectAnswers: '',
-    correctAnswer: '', 
-  };
-},
+  name: "App",
+  data() {
+    return {
+      question: "",
+      incorrectAnswers: "",
+      correctAnswer: "",
+      chose_answer: undefined
+    };
+  },
 
-computed:{
-  answers(){
-    var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
-    answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
-    return answers;
-  }
-},
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      );
+      return answers;
+    },
+  },
 
-created() {
-  this.axios
-    .get('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy')
-    .then((response) => {
-      if (response.data.results && response.data.results.length > 0) {
-        const result = response.data.results[0];
-        if (result.question && result.incorrect_answers && result.correct_answer) {
-          this.question = result.question;
-          this.incorrectAnswers = result.incorrect_answers;
-          this.correctAnswer = result.correct_answer;
-          console.log(response.data);
+  created() {
+    this.axios
+      .get("https://opentdb.com/api.php?amount=1&category=18&difficulty=easy")
+      .then((response) => {
+        if (response.data.results && response.data.results.length > 0) {
+          const result = response.data.results[0];
+          if (
+            result.question &&
+            result.incorrect_answers &&
+            result.correct_answer
+          ) {
+            this.question = result.question;
+            this.incorrectAnswers = result.incorrect_answers;
+            this.correctAnswer = result.correct_answer;
+            console.log(response.data);
+          } else {
+            console.log("Invalid response format");
+          }
         } else {
-          console.log('Invalid response format');
+          console.log("No results found");
         }
-      } else {
-        console.log('No results found');
-      }
-    })
-    .catch((error) => {
-      console.log('API request failed:', error);
-    });
-}
-
-
-  //url para o serviço
-  //https://opentdb.com/api.php?amount=1&category=18&difficulty=easy
-}
+      })
+      .catch((error) => {
+        console.log("API request failed:", error);
+      });
+  },
+};
 </script>
 
 <style lang="scss">
@@ -69,11 +76,11 @@ created() {
   max-width: 960px;
   margin: 60px auto;
 
-  input[type=radio]{
+  input[type="radio"] {
     margin: 12px 4px;
   }
 
-  button.send{
+  button.send {
     margin-top: 12px;
     height: 40px;
     min-width: 120px;
